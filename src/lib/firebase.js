d00fd4fb70eb5e9c9483700e23e859423d3e0d1e
Maria V <maria.villegas.pro@gmail.com>
2021-07-15 19:55:33 +0200

    fix: test git cached src/lib

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,7 +6,12 @@ import 'firebase/auth';
 // import { seedDatabase } from '../seed';
 
 const config = {
-  /*YOUR FIREBASE CONFIG */
+  apiKey: 'AIzaSyBbDJV7xpUR3zSCLfT1iRc_B772mNPVku0',
+  authDomain: 'instagram-tu.firebaseapp.com',
+  projectId: 'instagram-tu',
+  storageBucket: 'instagram-tu.appspot.com',
+  messagingSenderId: '555378168651',
+  appId: '1:555378168651:web:eb32767b146b9aff200ece'
 };
 
 const firebase = Firebase.initializeApp(config);
